890d9f20a716cb2604d07b89408e1a5151d8bb59
Sebastian Luna <110370421+slunadev@users.noreply.github.com>
2022-08-06 23:37:17 -0500

    Add files via upload
    Open-bootcamp

---
+++ funcion.java
@@ -0,0 +1,13 @@
+public class funcion {
+
+   public static int suma(int a, int b){
+        int op = a + b;
+    return op;
+    }
+
+    public static void main(String[] args) {
+        int resultado = suma(2, 8);
+        System.out.println("La suma es: " + resultado);
+    }
+}
+
